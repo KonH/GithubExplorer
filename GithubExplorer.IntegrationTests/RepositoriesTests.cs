98d4a46aeadcd3461427574e4bf17ed10f67d958
KonH <konh@yandex.ru>
2020-11-13 18:32:14 +0700

    First prototype

---
+++ GithubExplorer.IntegrationTests/RepositoriesTests.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace GithubExplorer.IntegrationTests {
+	public sealed class RepositoriesTests {
+		const string UserName = "KonH";
+
+		[Test]
+		public async Task IsRepositoriesFound() {
+			var explorer = new Explorer();
+
+			var repositories = (await explorer.GetRepositories(UserName)).ToArray();
+
+			repositories.Should().NotBeEmpty();
+			repositories.Should().Contain("konh.github.io");
+		}
+	}
+}
---
+++ GithubExplorer/Explorer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace GithubExplorer {
+	public sealed class Explorer {
+		readonly GitHubClient _client;
+
+		public Explorer() {
+			_client = new GitHubClient(new ProductHeaderValue("KonH-GithubExplorer"));
+		}
+
+		async Task<User> GetUser(string userName) =>
+			await _client.User.Get(userName);
+
+		public async Task<IEnumerable<string>> GetRepositories(string userName) {
+			var user         = await GetUser(userName);
+			var repositories = await _client.Repository.GetAllForUser(user.Login);
+			return repositories
+				.Select(r => r.Name);
+		}
+	}
+}
